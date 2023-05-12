614c513292b36b6504adcdcac3df0365d55c5f8f
Stijn Hendriks <stijnhendriks@protonmail.com>
2023-05-12 15:32:09 +0200

    Stable colors werkt nu stabiel

--- cam_test.py
+++ cam_test.py
@@ -1,37 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Mask R-CNN Demo
-# 
-# A quick intro to using the pre-trained model to detect and segment objects.
-
-# In[1]:
-
-
 import os
 import sys
-import random
-import math
-import numpy as np
-import skimage.io
-import matplotlib
-import matplotlib.pyplot as plt
 import cv2
 import time
+
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+
+import samples.coco.coco as coco
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn import config
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
+
 # Import COCO config
 sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-import samples.coco.coco as coco
-    
+
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
@@ -44,22 +30,6 @@ if not os.path.exists(COCO_MODEL_PATH):
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
-
-# In[2]:
-
-
-MODEL_DIR
-
-
-# ## Configurations
-# 
-# We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
-# 
-# For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
-
-# In[3]:
-
-
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
@@ -70,39 +40,11 @@ config = InferenceConfig()
 config.display()
 
 
-# ## Create Model and Load Trained Weights
-
-# In[4]:
-
-
 # Create model object in inference mode.
 model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
 # Load weights trained on MS-COCO
 model.load_weights(COCO_MODEL_PATH, by_name=True)
-
-
-# ## Class Names
-# 
-# The model classifies objects and returns class IDs, which are integer value that identify each class. Some datasets assign integer values to their classes and some don't. For example, in the MS-COCO dataset, the 'person' class is 1 and 'teddy bear' is 88. The IDs are often sequential, but not always. The COCO dataset, for example, has classes associated with class IDs 70 and 72, but not 71.
-# 
-# To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
-# 
-# To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
-# ```
-# # Load COCO dataset
-# dataset = coco.CocoDataset()
-# dataset.load_coco(COCO_DIR, "train")
-# dataset.prepare()
-# 
-# # Print class names
-# print(dataset.class_names)
-# ```
-# 
-# We don't want to require you to download the COCO dataset just to run this demo, so we're including the list of class names below. The index of the class name in the list represent its ID (first class is 0, second is 1, third is 2, ...etc.)
-
-# In[5]:
-
 
 # COCO Class names
 # Index of the class in the list is its ID. For example, to get ID of
@@ -124,15 +66,8 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'teddy bear', 'hair drier', 'toothbrush']
 
 
-class_colors = visualize.nonrandom_colors(len(class_names))
-# ## Run Object Detection
 
-# In[6]:
-
-
-# In[ ]:
-
-
+colors = None
 stream = cv2.VideoCapture(0)
 
 while True:
@@ -149,11 +84,9 @@ while True:
     class_ids = r['class_ids']
     scores = r['scores']
 
-    colors = [class_colors[i] for i in class_ids]
-
     # Run detection
     start = time.time()
-    masked_image = visualize.display_instances(frame, boxes, masks, class_ids, class_names, scores, colors=colors)
+    masked_image, colors = visualize.display_instances(frame, boxes, masks, class_ids, class_names, scores, colors=colors, stable_colors=True)
     end = time.time()
     print("Inference time: {:.2f}s".format(end - start))
 
@@ -165,10 +98,6 @@ while True:
 # When everything done, release the capture
 stream.release()
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
 
 
 
--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -57,18 +57,23 @@ def display_images(images, titles=None, cols=4, cmap=None, norm=None,
         i += 1
     plt.show()
 
+def create_color_class_mapping(colors, class_ids):
+    if isinstance(colors, list):
+        colors = {class_id: colors[i] for i, class_id in enumerate(list(set(class_ids)))}
+        return colors
 
-def nonrandom_colors(N):
+    elif isinstance(colors, dict):
+        return colors
 
-    """
-    Generate random colors.
-    To get visually distinct colors, generate them in HSV space then
-    convert to RGB.
-    """
-    k = 5
-    colors = [c / k for c in range(k + 1)]
-    colors = [(r, g, b) for r in colors for g in colors for b in colors]
-    colors = random.sample(colors, N)
+    else:
+        raise NotImplemented(f'Only mappings for lists and dicts are implemented, and your type is: {type(colors)}.')
+
+def update_color_mapping(colors, class_ids):
+    new_classes = [class_id for class_id in set(class_ids) if class_id not in colors]
+    N = len(new_classes)
+    new_colors = random_colors(N, bright=True)
+    for i, class_id in enumerate(new_classes):
+        colors[class_id] = new_colors[i]
     return colors
 
 def random_colors(N, bright=True):
@@ -79,8 +84,6 @@ def random_colors(N, bright=True):
     """
     brightness = 1.0 if bright else 0.7
     hsv = [(i / N, 1, brightness) for i in range(N)]
-    #hsv = [(random.random(), 1, brightness) for i in range(N)]
-
     colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
     random.shuffle(colors)
     return colors
@@ -97,20 +100,12 @@ def apply_mask(image, mask, color, alpha=0.5):
     return image
 
 
-def lookup_color(class_ids):
-    """
-    :param class_ids: [num_instances]
-    :return: [colors] list of correct colors for each class
-    """
-
-
-
 
 def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
-                      colors=None, captions=None):
+                      colors=None, stable_colors=True, captions=None):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
@@ -123,6 +118,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     colors: (optional) An array or colors to use with each object
     captions: (optional) A list of strings to use as captions for each object
     """
+
     # Number of instances
     N = boxes.shape[0]
     if not N:
@@ -137,7 +133,20 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         auto_show = True
 
     # Generate random colors
-    colors = colors or random_colors(N)
+    if not colors:
+        # This is how usually colors are created. We use the HSV space to create the colors. This
+        # ensures bright and contrastive colors. However, when we detect new classes for the first time,
+        # these also need to be added. Therefore, we keep updating a set dict of class_id-color-combinations,
+        # as you can see below. Note: we cannot simply generate all colors beforehand this way for all
+        # possible coco-classes, because this would create a lot of bad colors (e.g. low contrast,
+        # or unattractive colors). If you have a low number of total classes, prior generation of colors is
+        # probably the cleanest way to do this.
+        colors = random_colors(N)
+
+    if stable_colors:
+        colors = create_color_class_mapping(colors, class_ids)
+        colors = update_color_mapping(colors, class_ids)
+
 
     # Show area outside image boundaries.
     height, width = image.shape[:2]
@@ -148,7 +157,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
-        color = colors[i]
+        class_id = class_ids[i]
+        color = colors[class_id]
 
         # Bounding box
         if not np.any(boxes[i]):
@@ -191,99 +201,9 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     ax.imshow(masked_image.astype(np.uint8))
     if auto_show:
         plt.show()
-        
-def display_instances_cam(image, boxes, masks, class_ids, class_names,
-                      scores=None, title="",
-                      figsize=(16, 16), ax=None,
-                      show_mask=True, show_bbox=True,
-                      colors=None, captions=None):
-    """
-    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
-    masks: [height, width, num_instances]
-    class_ids: [num_instances]
-    class_names: list of class names of the dataset
-    scores: (optional) confidence scores for each box
-    title: (optional) Figure title
-    show_mask, show_bbox: To show masks and bounding boxes or not
-    figsize: (optional) the size of the image
-    colors: (optional) An array or colors to use with each object
-    captions: (optional) A list of strings to use as captions for each object
-    """
-    # Number of instances
-    N = boxes.shape[0]
-    if not N:
-        print("\n*** No instances to display *** \n")
-    else:
-        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
 
-    # If no axis is passed, create one and automatically call show()
-    auto_show = False
-    if not ax:
-        _, ax = plt.subplots(1, figsize=figsize)
-        auto_show = True
-
-    # Generate random colors
-    colors = colors or random_colors(N)
-
-    # Show area outside image boundaries.
-    height, width = image.shape[:2]
-    ax.set_ylim(height + 10, -10)
-    ax.set_xlim(-10, width + 10)
-    ax.axis('off')
-    ax.set_title(title)
-
-    masked_image = image.astype(np.uint32).copy()
-    for i in range(N):
-        color = colors[i]
-
-        # Bounding box
-        if not np.any(boxes[i]):
-            # Skip this instance. Has no bbox. Likely lost in image cropping.
-            continue
-        y1, x1, y2, x2 = boxes[i]
-        if show_bbox:
-            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
-                                alpha=0.7, linestyle="dashed",
-                                edgecolor=color, facecolor='none')
-            ax.add_patch(p)
-
-        # Label
-        if not captions:
-            class_id = class_ids[i]
-            score = scores[i] if scores is not None else None
-            label = class_names[class_id]
-            caption = "{} {:.3f}".format(label, score) if score else label
-        else:
-            caption = captions[i]
-        ax.text(x1, y1 + 8, caption,
-                color='w', size=20, backgroundcolor="none")
-
-        # Mask
-        mask = masks[:, :, i]
-        if show_mask:
-            masked_image = apply_mask(masked_image, mask, color)
-
-        # Mask Polygon
-        # Pad to ensure proper polygons for masks that touch image edges.
-        padded_mask = np.zeros(
-            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-        padded_mask[1:-1, 1:-1] = mask
-        contours = find_contours(padded_mask, 0.5)
-        for verts in contours:
-            # Subtract the padding and flip (y, x) to (x, y)
-            verts = np.fliplr(verts) - 1
-            p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
-    ax.imshow(masked_image.astype(np.uint8))
-    ax.figure.canvas.draw()
-
-    # convert canvas to image
-    canvas = np.fromstring(ax.figure.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-    canvas = canvas.reshape(ax.figure.canvas.get_width_height()[::-1] + (3,))
-    canvas = cv2.cvtColor(canvas, cv2.COLOR_RGB2BGR)
-    canvas = cv2.resize(canvas, (512, 512))
-    
-    return masked_image, canvas
+    print(f'Printing the colors at the end: {colors}')
+    return masked_image, colors
 
 
 def display_differences(image,
